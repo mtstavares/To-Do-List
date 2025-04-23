0c99e2d585720abbe7b723edc178ed4ad553d1ff
Matheus Tavares <mts.tavares01@gmail.com>
2025-04-23 14:59:05 -0300

    desisti de fazer funcionar com o render

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://to-do-list-6766.onrender.com/api/tasks';
+const apiUrl = 'http://localhost:5000/api/tasks';
 
 const taskForm = document.getElementById('task-form');
 const taskInput = document.getElementById('task-input');
